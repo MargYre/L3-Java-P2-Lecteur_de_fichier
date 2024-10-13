4d4776c48b94b14938210ee5510802d498241436
margaux <margaux.tarayre@gmail.com>
2024-10-13 21:05:18 +0200

    Finalisations des functions dans TextFileReaser.java

--- TextFileReader.java
+++ TextFileReader.java
@@ -28,10 +28,16 @@ public class TextFileReader implements FileReaderInterface {
     }
     @Override
     public void displayFileInReverse() {
-    
+        String[] lignes = contenu.toString().split("\n");
+        for (int i = lignes.length - 1; i >= 0; i--) {
+            System.out.println(lignes[i]);
+        }
     }
     @Override
     public void displayFilePalindromic() {
-    
+        for (int i = 0; i < contenu.length(); i++) {
+            System.out.print(contenu.charAt(contenu.length() - 1 - i));
+        }
+        System.out.println();
     }
 }
