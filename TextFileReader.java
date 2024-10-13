5846b56e3398c69ee7e7316f57d9f398f7e44d41
margaux <margaux.tarayre@gmail.com>
2024-10-13 21:36:52 +0200

    Modification de TextFileReader pour qu'il hérite de AbstractFileReader

--- TextFileReader.java
+++ TextFileReader.java
@@ -5,23 +5,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
-public class TextFileReader implements FileReaderInterface {
-    private StringBuilder contenu;
+public class TextFileReader extends AbstractFileReader {
     public TextFileReader() {
-        contenu = new StringBuilder();
+        super();
     }
 
-    @Override
-    public void readFile(String filePath) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String ligne;
-            while ((ligne = br.readLine()) != null) {
-                contenu.append(ligne).append("\n");
-            }
-            } catch (IOException e) {
-                System.out.println("Erreur lors de la lecture du fichier : " + e.getMessage());
-            }
-        }
     @Override
     public void displayFile() {
         System.out.println(contenu.toString());
