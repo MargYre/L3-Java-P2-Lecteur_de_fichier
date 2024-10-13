cd947776763e337f410c813b5c5ea32f5b1c604b
margaux <margaux.tarayre@gmail.com>
2024-10-13 21:59:10 +0200

    Ajout de fichier pour tester

--- FileReader.java
+++ FileReader.java
@@ -1,22 +1,36 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-
 public class FileReader {
-    public static void main(String[] args) throws IOException {
-        File f = new File("plop.txt");
-        try {
-            FileInputStream in = new FileInputStream(f);
-            int i = in.read();
-            while (i != -1) {
-                System.out.print((char) i);
-                i = in.read();
-            }
-            in.close();
-        } catch (Exception e) {
-            e.getStackTrace();
+    public static void main(String[] args) {
+        TextFileReader lecteurTxt = new TextFileReader();
+        
+        System.out.println("Lecture du fichier texte :");
+        lecteurTxt.readFile("files/plop.txt");
+        
+        System.out.println("Contenu du fichier à l'endroit:");
+        lecteurTxt.displayFile();
+
+        System.out.println("\nContenu du fichier à l'envers:");
+        lecteurTxt.displayFileInReverse();
+
+        System.out.println("\nContenu du fichier en mode palindromique:");
+        lecteurTxt.displayFilePalindromic();
+
+        CsvFileReader lecteurCsv = new CsvFileReader();
+
+        System.out.println("\nLecture du fichier CSV :");
+        lecteurCsv.readFile("files/fichier.csv");
+
+        System.out.println("Contenu du fichier CSV :");
+        lecteurCsv.displayFile();
+
+        TextFileReader lecteurTxt2 = new TextFileReader();
+        lecteurTxt2.readFile("monFichier2.txt");
+        
+        System.out.println("\nComparaison des fichiers texte :");
+        boolean sontIdentiques = lecteurTxt.compareFiles(lecteurTxt2);
+        if (sontIdentiques) {
+            System.out.println("Les fichiers sont identiques.");
+        } else {
+            System.out.println("Les fichiers sont différents.");
         }
     }
-
 }
