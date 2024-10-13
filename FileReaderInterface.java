fa580e2a6fca921cf4852e22784e37951189ece0
margaux <margaux.tarayre@gmail.com>
2024-10-13 20:37:28 +0200

    ajout d'une interface de lecteur de fichiers

---
+++ FileReaderInterface.java
@@ -0,0 +1,6 @@
+public interface FileReaderInterface {
+    public void readFile(String filePath);
+    public void displayFile();
+    public void displayFileInReverse();
+    public void displayFilePalindromic();
+}
